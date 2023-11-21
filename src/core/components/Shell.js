6317a3a150ac64b20a0e2705c86427e38c55f0e3
Christopher Shepherd <moi@cshepherd.fr>
2023-11-20 19:09:11 -0500

    fix: Respect theme if passed as prop

--- src/core/components/Shell.js
+++ src/core/components/Shell.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import './styles/Shell.css';
 import './styles/DarkTheme.css';
--- src/core/containers/Shell.js
+++ src/core/containers/Shell.js
@@ -4,7 +4,7 @@ import StoreContext from '../stores/StoreContext';
 import { setTheme, setLayout } from '../actions/Settings';
 
 export default connect(
-  ({ settings}) => ({ theme: settings.theme, layout: settings.layout}),
+  ({settings}, ownProps) => ({ layout: settings.layout, theme: ownProps.theme !== undefined ? ownProps.theme : settings.theme }),
   { setTheme, setLayout },
   null,
   { context: StoreContext })(Shell);
